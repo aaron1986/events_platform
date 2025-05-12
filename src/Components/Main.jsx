14c6632cfe1acae34dce13cd2abdfb045a904e84
aaron1986 <aaronbrucesmith86@gmail.com>
2025-05-12 15:44:18 +0100

    error using .env

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from 'react';
 
 export default function Main() {
   const [events, setEvents] = useState([]);
-  
+ 
   useEffect(() => {
-    
     const fetchData = async () => {
-        const apiKey = process.env.REACT_APP_TICKETMASTER_API_KEY;
       try {
+        const apiKey = process.env.REACT_APP_TICKETMASTER_API_KEY;
         const result = await fetch(
           // 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=FzhugPNAJdDZM7NJLZN1Pyh2FLwzPu2m&city=London'
           `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&city=London`
