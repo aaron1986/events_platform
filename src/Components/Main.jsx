a7a8116b314e469f907f3ea4890f85fc8b6c7ee3
aaron1986 <aaronbrucesmith86@gmail.com>
2025-05-12 16:39:03 +0100

    fixed .env error

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,29 +2,30 @@ import React, { useEffect, useState } from 'react';
 
 export default function Main() {
   const [events, setEvents] = useState([]);
- 
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const result = await fetch(
-           //'https://app.ticketmaster.com/discovery/v2/events.json?apikey=FzhugPNAJdDZM7NJLZN1Pyh2FLwzPu2m&city=London'
-          `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${process.env.REACT_APP_TICKETMASTER_API_KEY}&city=London`
+        const apiKey = import.meta.env.VITE_TICKETMASTER_API_KEY;
+        const response = await fetch(
+          `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&city=London`
         );
-        const data = await result.json();
+        const data = await response.json();
         const eventsData = data._embedded?.events || [];
         setEvents(eventsData);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-    
+
     fetchData();
   }, []);
- 
+
   return (
     <div>
       <section>
         <h2>View Events</h2>
+        <button>View Results</button>
         <ul className="kkk">
           {events.map((event) => (
             <li key={event.id}>
