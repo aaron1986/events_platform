c5f90456dfc6e702b8e4bc8e4da7493a796caea0
aaron1986 <aaronbrucesmith86@gmail.com>
2025-05-12 11:57:02 +0100

    updated css code

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,6 @@
 export default function Footer() {
     return (
         <footer>
-        <div class="social_media-link">
-      </div>
-      <div class="copyright">	&copy; Aaron Bruce Smith. </div>
      </footer>
     )
 }
