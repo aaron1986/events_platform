d157f779ac08d97768dea85a9c9f9b51b2678912
aaron1986 <aaronbrucesmith86@gmail.com>
2025-05-12 15:29:34 +0100

    updated .env file

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,10 +4,12 @@ export default function Main() {
   const [events, setEvents] = useState([]);
   
   useEffect(() => {
+    
     const fetchData = async () => {
         const apiKey = process.env.REACT_APP_TICKETMASTER_API_KEY;
       try {
         const result = await fetch(
+          // 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=FzhugPNAJdDZM7NJLZN1Pyh2FLwzPu2m&city=London'
           `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}&city=London`
         );
         const data = await result.json();
@@ -17,10 +19,10 @@ export default function Main() {
         console.error('Error fetching data:', error);
       }
     };
-
+    
     fetchData();
   }, []);
-
+ 
   return (
     <div>
       <section>
